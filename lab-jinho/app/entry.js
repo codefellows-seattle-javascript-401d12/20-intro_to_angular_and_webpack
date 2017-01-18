db3cc9a36b9891ce3c055b011db3ab84db7fd9ab
JINHO PAIK <JMPAIK83@GMAIL.COM>
2017-01-18 10:37:42 -0800

    added babel dependencies and edited webpack config

--- lab-jinho/app/entry.js
+++ lab-jinho/app/entry.js
@@ -8,22 +8,54 @@ const cowsay = require('cowsay-browser');
 
 const cowsayApp = angular.module('cowsayApp', []);
 
-cowsayApp.controller('CowsayController', ['$log', '$scope', CowsayController]);
+cowsayApp.controller('CowsayController', ['$log', CowsayController]);
 
-function CowsayController($log, $scope) {
+function CowsayController($log) {
   $log.debug('CowsayController');
 
-  let cowsayCtrl = $scope.cowsayCtrl= {};
+  this.title = 'Welcome to Cowville!';
+  this.history = [];
 
-  cowsayCtrl.title = 'Overheating Inflated Cowville!';
+  cowsay.list((err, cowfiles) => {
+    this.cowfiles = cowfiles;
+    this.current = this.cowfiles[0];
+  });
 
-  cowsayCtrl.speak = function(input) {
+  this.update = function(input) {
+    $log.debug('cowsayCtrl.update()');
+    return cowsay.say({ text: input || 'moooooooo', f: this.current });
+  };
+
+  this.speak = function(input) {
     $log.debug('cowsayCtrl.speak()');
-    return cowsay.say({ text: input || 'moooooooo' });
+    this.spoken = this.update(input);
+    this.history.push(this.spoken);
   };
 
-  cowsayCtrl.logger = function(input) {
-    $log.debug('cowsayCtrl.logger()');
-    $log.log(input);
+  this.undo = function() {
+    $log.debug('cowsayCtrl.undo()');
+    this.history.pop();
+    this.spoken = this.history.pop() || '';
   };
+}
+
+cowsayApp.controller('NavController', ['$log', NavController]);
+
+function NavController($log) {
+  $log.debug('NavController');
+
+  this.routes = [
+    {
+      name: 'home',
+      url: '/home'
+    },
+    {
+      name: 'about',
+      url: '/about-us'
+    },
+    {
+      name: 'contact',
+      url: '/contact-us'
+    }
+  ];
 }
