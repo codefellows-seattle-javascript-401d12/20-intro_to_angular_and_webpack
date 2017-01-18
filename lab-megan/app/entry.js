6b7c9b89b29e0677a47fb2ff484e118c62b44a3c
Megan Reardon <meganr@gmail.com>
2017-01-18 09:10:44 -0800

    updated entry.js added babel file fixed bug

--- lab-megan/app/entry.js
+++ lab-megan/app/entry.js
@@ -8,21 +8,73 @@ const cowsay = require('cowsay-browser');
 
 const cowsayApp = angular.module('cowsayApp', []);
 
-cowsayApp.controller('CowsayAppController', ['$log', '$scope', CowsayAppController]);
+cowsayApp.controller('CowsayAppController', ['$log', CowsayAppController]);
 
-function CowsayAppController($log, $scope) {
-  $log.debug('CowsayAppController');
-  let cowsayAppCtrl = $scope.cowsayAppCtrl = {};
+function CowsayAppController($log) {
+  $log.debug('CowsayAppControlller');
 
-  cowsayAppCtrl.title = 'Welcome to Cow Town!';
+  this.title = 'Welcome to Cow Town';
+  this.history = [];
 
-  cowsayAppCtrl.speak = function(input) {
-    $log.debug('cowsayAppCtrl.speak()');
-    return cowsay.say({ text: input || 'hissss' });
+  cowsay.list((err, cowfiles) => {
+    this.cowfiles = cowfiles[0];
+  });
+
+  this.update = function(input) {
+    $log.debug('cowsayAppCtrl.update()');
+    return cowsay.say({ text: input || 'hissss', f: this.current });
   };
 
-  cowsayAppCtrl.logger = function(input) {
-    $log.debug('cowsayAppCtrl.logger()');
-    $log.log(input);
+  this.speak = function(input) {
+    $log.debug('cowsayAppCtrl.speak()');
+    this.spoken = this.update(input);
+    this.history.push(this.spoken);
+  };
+
+  this.undo = function() {
+    $log.debug('cowsayAppCtrl.undo()');
+    this.history.pop();
   };
 }
+
+cowsayApp.controller('NavAppController', ['$log', NavAppController]);
+
+function NavAppController($log) {
+  $log.debug('NavAppController');
+
+  this.routes = [
+    {
+      name: 'home',
+      url: '/home'
+    },
+    {
+      name: 'about',
+      url: '/about-us'
+    },
+    {
+      name: 'contact',
+      url: '/contact-us'
+    }
+  ];
+}
+
+
+// code from Lab 20
+// cowsayApp.controller('CowsayAppController', ['$log', '$scope', CowsayAppController]);
+//
+// function CowsayAppController($log, $scope) {
+//   $log.debug('CowsayAppController');
+//   let cowsayAppCtrl = $scope.cowsayAppCtrl = {};
+//
+//   cowsayAppCtrl.title = 'Welcome to Cow Town!';
+//
+//   cowsayAppCtrl.speak = function(input) {
+//     $log.debug('cowsayAppCtrl.speak()');
+//     return cowsay.say({ text: input || 'hissss' });
+//   };
+//
+//   cowsayAppCtrl.logger = function(input) {
+//     $log.debug('cowsayAppCtrl.logger()');
+//     $log.log(input);
+//   };
+// }
