960efe43e1150cf9665ad1801c0b5e8869a87277
Kyler Dotson <kyler.dotson@gmail.com>
2017-01-17 17:40:28 -0800

    cowfile dropdown appears

--- lab-kyler/app/entry.js
+++ lab-kyler/app/entry.js
@@ -12,9 +12,27 @@ const cowsayApp = angular.module('cowsayApp', []);
 cowsayApp.controller('cowsayController', ['$log', '$scope', cowsayController]);
 
 function cowsayController($log) {
-  $log.debug('CowsayController');
+  $log.debug('cowsayController constructor');
 
   this.title = 'Welcome to Cowville!';
+  this.history = [];
+  this.text = '';
+
+  cowsay.list( (err, cowfiles) => {
+    this.cowfiles = cowfiles;
+    this.current = this.cowfiles[0];
+  });
+
+  this.undo = function() {
+    $log.debug('cowsayCtrl.undo()');
+    this.spoken = this.history.pop();
+  };
+
+  this.update = function(input) {
+    $log.debug('cowsayCtrl.update()');
+
+    return cowsay.say({ text: input || 'no input', f: this.current });
+  };
 
   this.speak = function(input) {
     $log.debug('cowsayCtrl.speak()');
