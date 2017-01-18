4cbc254549fc1926b8736236204b397cd10ff3d7
Geoff Simons <geoff.simons@gmail.com>
2017-01-17 19:53:00 -0800

    Refactor to use 'controller as' syntax

--- app/entry.js
+++ app/entry.js
@@ -8,45 +8,42 @@ const cowsay = require('cowsay-browser');
 
 const cowsayApp = angular.module('cowsayApp', []);
 
-cowsayApp.controller('CowsayController', ['$log', '$scope', CowsayController]);
+cowsayApp.controller('CowsayController', ['$log', CowsayController]);
 
-function CowsayController($log, $scope) {
+function CowsayController($log) {
   $log.debug('CowsayCont');
 
-  let cowsayCtrl = $scope.cowsayCtrl = {};
-  cowsayCtrl.history = [];
+  this.title = 'Geoff\'s Cowville!';
+  this.history = [];
 
-  cowsayCtrl.title = 'Geoff\'s Cowville!';
-
-  cowsayCtrl.speak = function(input) {
+  this.speak = function(input) {
     $log.debug('cowsayCtrl.speak()');
     return cowsay.say({ text: input || 'MOOOOO-OOOO' });
     //Q: Can we set other params for cowsay?
     //Q: How do we connect those params to a value from the GUI?
   };
 
-  cowsayCtrl.logger = function(input) {
+  this.logger = function(input) {
     $log.debug('cowsayCtrl.logger()');
     $log.log(input);
   };
 
-  cowsayCtrl.save = function() {
-    $log.debug('cowsayCtrl.save()', cowsayCtrl.text);
-    if(cowsayCtrl.text) {
-      cowsayCtrl.history.push(cowsayCtrl.text);
-      cowsayCtrl.text;
+  this.save = function() {
+    $log.debug('cowsayCtrl.save()', this.text);
+    if(this.text) {
+      this.history.push(this.text);
     }
   };
 
-  cowsayCtrl.last = function() {
-    $log.debug('cowsayCtrl.last()');
-    let len = cowsayCtrl.history.length;
+  this.last = function() {
+    $log.debug('cowsayCtrl.last()', this.history);
+    let len = this.history.length;
     if(len == 0) return;
-    return cowsayCtrl.history[len - 1];
+    return this.history[len - 1];
   };
 
-  cowsayCtrl.back = function() {
+  this.back = function() {
     $log.debug('cowsayCtrl.back()');
-    cowsayCtrl.history.pop();
+    this.history.pop();
   };
 }
