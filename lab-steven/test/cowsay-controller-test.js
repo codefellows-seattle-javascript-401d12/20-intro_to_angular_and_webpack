4f63aaf4990d40d08d2ab3956e7659378bda88a2
Steven Bateman <batemanvo@gmail.com>
2017-01-18 17:55:15 -0800

    Successfully tested first initial property.

--- lab-steven/test/cowsay-controller-test.js
+++ lab-steven/test/cowsay-controller-test.js
@@ -1,1 +1,19 @@
 'use strict';
+
+require('./lib/test-helper.js');
+
+const angular = require('angular');
+const cowsay = require('cowsay-browser');
+
+describe('Cowsay Controller', function() {
+  beforeEach(() => {
+    angular.mock.module('cowApp');
+    angular.mock.inject($controller => this.cowsayCtrl = new $controller('CowsayController'));
+  });
+
+  describe('Initial properties', () => {
+    it('Title should equal 401 JS Cowsay App For Lab 21', () => {
+      expect(this.cowsayCtrl.title).toEqual('401 JS Cowsay App For Lab 21');
+    });
+  });
+});
