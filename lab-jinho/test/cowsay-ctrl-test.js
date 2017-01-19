fbadaec7abd19ea78c6f3570e84f4798c7afea13
JINHO PAIK <JMPAIK83@GMAIL.COM>
2017-01-18 21:29:47 -0800

    added karma tests

--- lab-jinho/test/cowsay-ctrl-test.js
+++ lab-jinho/test/cowsay-ctrl-test.js
@@ -25,7 +25,36 @@ describe('Cowsay Controller', function() {
     it('list of cowfiles should show proper cowfiles', () => {
       cowsay.list((err, list) => {
         expect(this.cowsayCtrl.cowfiles).toEqual(list);
-      })
-    })
-  })
+        expect(this.cowsayCtrl.current).toEqual(list[0]);
+      });
+    });
+  });
+
+  describe('#update', () => {
+    it('should return a cow that says testing', () => {
+      let expected = cowsay.say({ text: 'testing', f: this.cowsayCtrl.current });
+      let result = this.cowsayCtrl.update('testing');
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('#speak', () => {
+    it('should return a cow that says testing', () => {
+      let expected = cowsay.say({ text: 'testing', f: this.cowsayCtrl.current });
+      this.cowsayCtrl.speak('testing');
+      expect(this.cowsayCtrl.spoken).toEqual(expected);
+      expect(this.cowsayCtrl.history[0]).toEqual(expected);
+    });
+  });
+
+  describe('#undo', () => {
+    it('should return a cow that says testing', () => {
+      let expected = cowsay.say({ text: 'testing', f: this.cowsayCtrl.current });
+      this.cowsayCtrl.speak('testing');
+      this.cowsayCtrl.speak('testing again');
+      this.cowsayCtrl.undo();
+      expect(this.cowsayCtrl.spoken).toEqual(expected);
+      expect(this.cowsayCtrl.history.length).toEqual(0);
+    });
+  });
 });
